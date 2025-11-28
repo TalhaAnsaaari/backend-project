07c7d2ed218ad71910e3b68f7f16a00ccd7c4bfe
TalhaAnsaaari <talha14804@gmail.com>
2025-11-28 15:04:04 +0530

    All setup configured

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,10 +54,11 @@ const userSchema = new Schema(
 )
 
 userSchema.pre("save",async function(next){
-    if(this.isModified("password")){
-        this.password = bcrypt.hash(this.password,10)
+    if(!this.isModified("password")) return next();
+    
+        this.password = await bcrypt.hash(this.password,10)
         next()
-    }
+    
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
